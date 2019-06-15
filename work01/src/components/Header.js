6321e6d8b43076ae795b6b7d9ce0149efd7836ee
謝政宏 <b3194359@gmail.com>
2019-06-15 23:53:08 +0800

    navbar 樣式

--- work01/src/components/Header.js
+++ work01/src/components/Header.js
@@ -26,6 +26,16 @@ class Header extends React.Component {
 						<li><a href="">採用情報</a></li>
 					</ul>
 				</nav>
+				<div className="global-nav">
+					<ul>
+						<li><a href="">ご紹介され続ける理由</a></li>
+						<li><a href="">建築事例</a></li>
+						<li><a href="">お客様の声</a></li>
+						<li><a href="">モデルハウス</a></li>
+						<li><a href="">HAKUAIの家づくり</a></li>
+						<li><a href="">家づくりのメニュー</a></li>
+					</ul>
+				</div>
 				<div className="fixed-wrap">
 					<div className="box">
 						<a href="javascript::" className="info-mark">
--- work01/src/components/Home.js
+++ work01/src/components/Home.js
@@ -4,12 +4,7 @@ import React from 'react';
 class Home extends React.Component{
    render (){
       return(
-        <div className="bio">
-          <h1 className="name">Home</h1>
-          <h2 className="location">Taichung, Taiwan</h2>
-          <div className="occupation">
-            <p>Protecting Freedom @ onemonthedu</p>
-          </div>
+        <div>
         </div>
       )
    }
