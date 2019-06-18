10ff65215e668bea36ca6074a235e86f8fee671f
謝政宏 <b3194359@gmail.com>
2019-06-18 20:38:46 +0800

    no job no life

--- smiple-calculator/src/components/ButtonPanel.js
+++ smiple-calculator/src/components/ButtonPanel.js
@@ -1,52 +1,52 @@
 import React from 'react';
-import Button from "./Button";
-import PropTypes from "prop-types";
+import Button from './Button';
+import PropTypes from 'prop-types';
 
 export default class ButtonPanel extends React.Component {
 	static propTypes = {
-    onButtonPress: PropTypes.func,
-  };
+		onButtonPress: PropTypes.func
+	};
 
-  onButtonPress = buttonName => {
-    this.props.onButtonPress(buttonName);
-  };
-
+	onButtonPress = (buttonName) => {
+		this.props.onButtonPress(buttonName);
+	};
 
 	render() {
 		const buttonData = [
-				{text: "M+", type: ""},
-				{text: "M-", type: ""},
-				{text: "MR", type: ""},
-				{text: "Ben's Calc", type: ""},
-			  {text: "C", type: "primary"},
-			  {text: '⇦', type: "primary"},
-			  {text: "%", type: "operator"},
-			  {text: "/", type: "operator"},
-			  {text: "7", type: ""},
-			  {text: "8", type: ""},
-			  {text: "9", type: ""},
-			  {text: "*", type: "operator"},
-			  {text: "4", type: ""},
-			  {text: "5", type: ""},
-			  {text: "6", type: ""},
-			  {text: "-", type: "operator"},
-				{text: "1", type: ""},
-			  {text: "2", type: ""},
-			  {text: "3", type: ""},
-			  {text: "+", type: "operator"},
-				{text: "0", type: ""},
-			  {text: ".", type: ""},
-			  {text: "=", type: "large"}
+			{ text: 'M+', type: '' },
+			{ text: 'M-', type: '' },
+			{ text: 'MR', type: '' },
+			{ text: "Ben's Calc", type: '' },
+			{ text: 'C', type: 'primary' },
+			{ text: '⇦', type: 'primary' },
+			{ text: '%', type: 'operator' },
+			{ text: '/', type: 'operator' },
+			{ text: '7', type: '' },
+			{ text: '8', type: '' },
+			{ text: '9', type: '' },
+			{ text: '*', type: 'operator' },
+			{ text: '4', type: '' },
+			{ text: '5', type: '' },
+			{ text: '6', type: '' },
+			{ text: '-', type: 'operator' },
+			{ text: '1', type: '' },
+			{ text: '2', type: '' },
+			{ text: '3', type: '' },
+			{ text: '+', type: 'operator' },
+			{ text: '0', type: '' },
+			{ text: '.', type: '' },
+			{ text: '=', type: 'large' }
 		];
 
 		return (
 			<section className="key">
 				<div className="keypanel__row">
-				  {
-						buttonData.map(button => <Button type={button.type} onButtonPress={this.props.onButtonPress}>{button.text}</Button>)
-					}
+					{buttonData.map((button) => (
+						<Button type={button.type} onButtonPress={this.props.onButtonPress}>
+							{button.text}
+						</Button>
+					))}
 				</div>
-
 			</section>
 		);
 	}
--- work01/src/components/Home.js
+++ work01/src/components/Home.js
@@ -42,12 +42,106 @@ class Home extends React.Component {
 
 						<div className="News">
 							<div className="col-3">
-								
+								<p>WORKS</p>
+								<h2>
+									<img src="https://e-hakuai.com/img/top/sec1-ttl.png" alt="" />
+								</h2>
+								<p>
+									<div>博愛がこれまでに</div>
+									<div>手掛けてきた建築事例です。</div>
+								</p>
+								<div className="more-btn">
+									<a href="">
+										建築事例をもっと見る
+									</a>
+								</div>
 							</div>
 							<div className="col-9">
+								<article className="col-4">
+									<a href="">
+										<figure className="thumb">
+											<img src="https://e-hakuai.com/wp-content/uploads/2019/05/G7A0835-min-a-min-425x285.jpg" alt="" />
+										</figure>
+									</a>
+								</article>
+								<article className="col-4">
+									<a href="">
+										<figure className="thumb">
+											<img src="https://e-hakuai.com/wp-content/uploads/2019/04/G7A0456-2-425x285.jpg" alt="" />
+										</figure>
+									</a>
+								</article>
+								<article className="col-4">
+									<a href="">
+										<figure className="thumb">
+											<img src="https://e-hakuai.com/wp-content/uploads/2019/04/G7A6628-425x285.jpg" alt="" />
+										</figure>
+									</a>
+								</article>
+								<article className="col-4">
+									<a href="">
+										<figure className="thumb">
+											<img src="https://e-hakuai.com/wp-content/uploads/2019/04/G7A7656-425x285.jpg" alt="" />
+										</figure>
+									</a>
+								</article>
+								<article className="col-4">
+									<a href="">
+										<figure className="thumb">
+											<img src="https://e-hakuai.com/wp-content/uploads/2019/04/c70e992b58b5ec91f1e2c72c54fe5d88-425x285.jpg" alt="" />
+										</figure>
+									</a>
+								</article>
+								<article className="col-4">
+									<a href="">
+										<figure className="thumb">
+											<img src="https://e-hakuai.com/wp-content/uploads/2019/04/c70e992b58b5ec91f1e2c72c54fe5d88-425x285.jpg" alt="" />
+										</figure>
+									</a>
+								</article>
+								<article className="col-4">
+									<a href="">
+										<figure className="thumb">
+											<img src="https://e-hakuai.com/wp-content/uploads/2019/04/55c7982a6b8a59f0f69744c051ae0eaf-425x285.jpg" alt="" />
+										</figure>
+									</a>
+								</article>
+								<article className="col-4">
+									<a href="">
+										<figure className="thumb">
+											<img src="https://e-hakuai.com/wp-content/uploads/2019/04/c70e992b58b5ec91f1e2c72c54fe5d88-1-425x285.jpg" alt="" />
+										</figure>
+									</a>
+								</article>
+								<article className="col-4">
+									<a href="">
+										<figure className="thumb">
+											<img src="https://e-hakuai.com/wp-content/uploads/2019/04/hakuai-001-425x285.jpg" alt="" />
+										</figure>
+									</a>
+								</article>
 							</div>
 						</div>
 
+						<div className="Information">
+							<div className="info-title">
+								<h2>Information</h2>
+								<a class="view-btn" href="">ALL VIEW</a>
+							</div>
+							<div className="info-items">
+								<ul>
+									<li><a href="#">afadf</a></li>
+									<li><a href="#">afadf</a></li>
+									<li><a href="#">afadf</a></li>
+									<li><a href="#">afadf</a></li>
+									<li><a href="#">afadf</a></li>
+								</ul>
+							</div>
+
+							<div className="info-lists">
+
+							</div>
+						</div>
 					</section>
 				</div>
 			</main>
