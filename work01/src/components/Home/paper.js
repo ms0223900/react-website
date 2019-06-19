c27e70727e58df736c0788030da1415085efbd63
謝政宏 <b3194359@gmail.com>
2019-06-20 01:18:15 +0800

    會社概要 完成

--- work01/src/components/Home/paper.js
+++ work01/src/components/Home/paper.js
@@ -1,126 +1,134 @@
-import React from 'react'
+import React from 'react';
+
+export const paperTitle = ({ titleText = '', link='' , className='title' }) => (
+	<div className={ className }>
+		<div>{ titleText }</div>
+		<a href={ link } class="view-btn">
+			ALL VIEW
+		</a>
+	</div>
+)
 
 export default () => {
-  return (
-    <section id="paper">
-      <div className="col-12">
-        <div className="title">
-          <div>イベント情報</div>
-          <a href="" class="view-btn">ALL VIEW</a>
-        </div>
-        <div className="content">
-          <a className="col-6">
-            <img src="" alt="" />
-            <div className="box">
-              <div className="date">
-                2019.03.21<span>完成見學會</span>
-              </div>
-              <p>
-                6月29日・30日　早良区のモダン×趣味の家　完成内覧会
-                <span>預約制</span>
-                <span>New</span>
-              </p>
-            </div>
-          </a>
-        </div>
-      </div>
+	return (
+		<section id="paper">
+			<div className="col-12">
 
-      <div className="col-12">
-        <div className="col-6">
-          <div className="title">
-            <div>イベント情報</div>
-            <a href="" class="view-btn">ALL VIEW</a>
-          </div>
-          <div className="content">
-            <a className="item">
-              <img src="" alt="" />
-              <div className="box">
-                <div className="date">
-                  2019.03.21<span>完成見學會</span>
-                </div>
-                <p>
-                  6月29日・30日　早良区のモダン×趣味の家　完成内覧会
-                  <span>預約制</span>
-                  <span>New</span>
-                </p>
-              </div>
-            </a>
-            <a className="item">
-              <img src="" alt="" />
-              <div className="box">
-                <div className="date">
-                  2019.03.21<span>完成見學會</span>
-                </div>
-                <p>
-                  6月29日・30日　早良区のモダン×趣味の家　完成内覧会
-                  <span>預約制</span>
-                  <span>New</span>
-                </p>
-              </div>
-            </a>
-            <a className="item">
-              <img src="" alt="" />
-              <div className="box">
-                <div className="date">
-                  2019.03.21<span>完成見學會</span>
-                </div>
-                <p>
-                  6月29日・30日　早良区のモダン×趣味の家　完成内覧会
-                  <span>預約制</span>
-                  <span>New</span>
-                </p>
-              </div>
-            </a>
-          </div>
-        </div>
-        <div className="col-6">
-          <div className="title">
-            <div>イベント情報</div>
-            <a href="" class="view-btn">ALL VIEW</a>
-          </div>
-          <div className="content">
-            <a className="item">
-              <img src="" alt="" />
-              <div className="box">
-                <div className="date">
-                  2019.03.21<span>完成見學會</span>
-                </div>
-                <p>
-                  6月29日・30日　早良区のモダン×趣味の家　完成内覧会
-                  <span>預約制</span>
-                  <span>New</span>
-                </p>
-              </div>
-            </a>
-            <a className="item">
-              <img src="" alt="" />
-              <div className="box">
-                <div className="date">
-                  2019.03.21<span>完成見學會</span>
-                </div>
-                <p>
-                  6月29日・30日　早良区のモダン×趣味の家　完成内覧会
-                  <span>預約制</span>
-                  <span>New</span>
-                </p>
-              </div>
-            </a>
-            <a className="item">
-              <img src="" alt="" />
-              <div className="box">
-                <div className="date">
-                  2019.03.21<span>完成見學會</span>
-                </div>
-                <p>
-                  6月29日・30日　早良区のモダン×趣味の家　完成内覧会
-                  <span>預約制</span>
-                  <span>New</span>
-                </p>
-              </div>
-            </a>
-          </div>
-        </div>
-      </div>
-    </section>
-  )
-}
+				<paperTitle className={'title'} titleText={'イベント情報'}/>
+
+				<div className="content">
+					<a className="col-6">
+						<img src="" alt="" />
+						<div className="box">
+							<div className="date">
+								2019.03.21<span>完成見學會</span>
+							</div>
+							<p>
+								6月29日・30日　早良区のモダン×趣味の家　完成内覧会
+								<span>預約制</span>
+								<span>New</span>
+							</p>
+						</div>
+					</a>
+				</div>
+			</div>
+
+			<div className="col-12">
+
+				<div className="col-6">
+
+					<paperTitle className={'title'} titleText={'施工状況'}/>
+
+					<div className="content">
+						<a className="item">
+							<img src="" alt="" />
+							<div className="box">
+								<div className="date">
+									2019.03.21<span>完成見學會</span>
+								</div>
+								<p>
+									6月29日・30日　早良区のモダン×趣味の家　完成内覧会
+									<span>預約制</span>
+									<span>New</span>
+								</p>
+							</div>
+						</a>
+						<a className="item">
+							<img src="" alt="" />
+							<div className="box">
+								<div className="date">
+									2019.03.21<span>完成見學會</span>
+								</div>
+								<p>
+									6月29日・30日　早良区のモダン×趣味の家　完成内覧会
+									<span>預約制</span>
+									<span>New</span>
+								</p>
+							</div>
+						</a>
+						<a className="item">
+							<img src="" alt="" />
+							<div className="box">
+								<div className="date">
+									2019.03.21<span>完成見學會</span>
+								</div>
+								<p>
+									6月29日・30日　早良区のモダン×趣味の家　完成内覧会
+									<span>預約制</span>
+									<span>New</span>
+								</p>
+							</div>
+						</a>
+					</div>
+				</div>
+
+				<div className="col-6">
+
+					<paperTitle className={'title'} titleText={'オフィシャルブログ'}/>
+
+					<div className="content">
+						<a className="item">
+							<img src="" alt="" />
+							<div className="box">
+								<div className="date">
+									2019.03.21<span>完成見學會</span>
+								</div>
+								<p>
+									6月29日・30日　早良区のモダン×趣味の家　完成内覧会
+									<span>預約制</span>
+									<span>New</span>
+								</p>
+							</div>
+						</a>
+						<a className="item">
+							<img src="" alt="" />
+							<div className="box">
+								<div className="date">
+									2019.03.21<span>完成見學會</span>
+								</div>
+								<p>
+									6月29日・30日　早良区のモダン×趣味の家　完成内覧会
+									<span>預約制</span>
+									<span>New</span>
+								</p>
+							</div>
+						</a>
+						<a className="item">
+							<img src="" alt="" />
+							<div className="box">
+								<div className="date">
+									2019.03.21<span>完成見學會</span>
+								</div>
+								<p>
+									6月29日・30日　早良区のモダン×趣味の家　完成内覧会
+									<span>預約制</span>
+									<span>New</span>
+								</p>
+							</div>
+						</a>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+};
