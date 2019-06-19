1daef4f8914d2b1382f304a1fbf22e390ecc4c52
謝政宏 <b3194359@gmail.com>
2019-06-20 01:53:46 +0800

    會社概要 完成

--- work01/src/components/Home/paper.js
+++ work01/src/components/Home/paper.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export const paperTitle = ({ titleText = '', link='' , className='title' }) => (
+export const PaperTitle = ({ titleText = '', link='' , className='title' }) => (
 	<div className={ className }>
 		<div>{ titleText }</div>
 		<a href={ link } class="view-btn">
@@ -9,27 +9,38 @@ export const paperTitle = ({ titleText = '', link='' , className='title' }) => (
 	</div>
 )
 
+export const ItemLists = ({imgSrc = '', link = '', date = '', tag1 = '', tag2 = '', content = '', mark1 = '', mark2 = ''}) => (
+	<a href={link} className="item">
+		<img src={imgSrc} alt="" />
+		<div className="box">
+			<div className="date">
+				{date}<span>{tag1}</span><span>{tag2}</span>
+			</div>
+			<p>
+				{content}
+				<span>{mark1}</span>
+				<span>{mark2}</span>
+			</p>
+		</div>
+	</a>
+)
+
 export default () => {
 	return (
 		<section id="paper">
 			<div className="col-12">
 
-				<paperTitle className={'title'} titleText={'イベント情報'}/>
+				<PaperTitle className={'title'} titleText={'イベント情報'}/>
+
+
 
 				<div className="content">
-					<a className="col-6">
-						<img src="" alt="" />
-						<div className="box">
-							<div className="date">
-								2019.03.21<span>完成見學會</span>
-							</div>
-							<p>
-								6月29日・30日　早良区のモダン×趣味の家　完成内覧会
-								<span>預約制</span>
-								<span>New</span>
-							</p>
-						</div>
-					</a>
+					<ItemLists
+						date={'2019.03.22'}
+						tag1={'完成見學會'}
+						content={'6月29日・30日　早良区のモダン×趣味の家　完成内覧会'}
+						mark1={'預約制'}
+						mark2={'New'}/>
 				</div>
 			</div>
 
@@ -37,95 +48,31 @@ export default () => {
 
 				<div className="col-6">
 
-					<paperTitle className={'title'} titleText={'施工状況'}/>
+					<PaperTitle className={'title'} titleText={'施工状況'}/>
 
 					<div className="content">
-						<a className="item">
-							<img src="" alt="" />
-							<div className="box">
-								<div className="date">
-									2019.03.21<span>完成見學會</span>
-								</div>
-								<p>
-									6月29日・30日　早良区のモダン×趣味の家　完成内覧会
-									<span>預約制</span>
-									<span>New</span>
-								</p>
-							</div>
-						</a>
-						<a className="item">
-							<img src="" alt="" />
-							<div className="box">
-								<div className="date">
-									2019.03.21<span>完成見學會</span>
-								</div>
-								<p>
-									6月29日・30日　早良区のモダン×趣味の家　完成内覧会
-									<span>預約制</span>
-									<span>New</span>
-								</p>
-							</div>
-						</a>
-						<a className="item">
-							<img src="" alt="" />
-							<div className="box">
-								<div className="date">
-									2019.03.21<span>完成見學會</span>
-								</div>
-								<p>
-									6月29日・30日　早良区のモダン×趣味の家　完成内覧会
-									<span>預約制</span>
-									<span>New</span>
-								</p>
-							</div>
-						</a>
+						{[ ...Array(3).keys() ].map((arr) => <ItemLists
+																										key={arr}
+																										date={'2019.03.21'}
+																										tag1={'完成見學會'}
+																										content={'6月29日・30日　早良区のモダン×趣味の家　完成内覧会'}
+																										mark1={'預約制'}
+																										mark2={'New'}/>)}
 					</div>
 				</div>
 
 				<div className="col-6">
 
-					<paperTitle className={'title'} titleText={'オフィシャルブログ'}/>
+					<PaperTitle className={'title'} titleText={'オフィシャルブログ'}/>
 
 					<div className="content">
-						<a className="item">
-							<img src="" alt="" />
-							<div className="box">
-								<div className="date">
-									2019.03.21<span>完成見學會</span>
-								</div>
-								<p>
-									6月29日・30日　早良区のモダン×趣味の家　完成内覧会
-									<span>預約制</span>
-									<span>New</span>
-								</p>
-							</div>
-						</a>
-						<a className="item">
-							<img src="" alt="" />
-							<div className="box">
-								<div className="date">
-									2019.03.21<span>完成見學會</span>
-								</div>
-								<p>
-									6月29日・30日　早良区のモダン×趣味の家　完成内覧会
-									<span>預約制</span>
-									<span>New</span>
-								</p>
-							</div>
-						</a>
-						<a className="item">
-							<img src="" alt="" />
-							<div className="box">
-								<div className="date">
-									2019.03.21<span>完成見學會</span>
-								</div>
-								<p>
-									6月29日・30日　早良区のモダン×趣味の家　完成内覧会
-									<span>預約制</span>
-									<span>New</span>
-								</p>
-							</div>
-						</a>
+						{[ ...Array(3).keys() ].map((arr) => <ItemLists
+																										key={arr}
+																										date={'2019.03.21'}
+																										tag1={'完成見學會'}
+																										content={'6月29日・30日　早良区のモダン×趣味の家　完成内覧会'}
+																										mark1={'預約制'}
+																										mark2={'New'}/>)}
 					</div>
 				</div>
 			</div>
