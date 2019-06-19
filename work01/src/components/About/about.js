826b829f3f78f3cd42878659992d347d43ce50fe
謝政宏 <b3194359@gmail.com>
2019-06-19 22:08:09 +0800

    會社概要 完成

--- work01/src/components/About/about.js
+++ work01/src/components/About/about.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import Banner from './Banner';
+import Message from './Message';
+import Company from './companyIntro';
+import Member from './member';
+import Footers from '../Home/footer';
 
 class About extends React.Component {
 	render() {
 		return (
-			<main>
-				<div id="content">
-					<Banner />
-				</div>
-			</main>
+			<React.Fragment>
+				<main>
+					<div id="content">
+						<Banner />
+						<Message />
+						<Company />
+						<Member />
+					</div>
+				</main>
+				<Footers />
+			</React.Fragment>
 		);
 	}
 }
---
+++ work01/src/components/About/companyIntro.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+export default () => {
+	return (
+		<section id="company">
+			<div className="intro">
+				<div className="col-8">
+					<h2>会社概要</h2>
+				</div>
+				<table>
+					<tbody>
+						<tr>
+							<th>s</th>
+							<td>s</td>
+						</tr>
+						<tr>
+							<th>s</th>
+							<td>s</td>
+						</tr>
+						<tr>
+							<th>s</th>
+							<td>s</td>
+						</tr>
+						<tr>
+							<th>s</th>
+							<td>s</td>
+						</tr>
+						<tr>
+							<th>s</th>
+							<td>s</td>
+						</tr>
+						<tr>
+							<th>s</th>
+							<td>s</td>
+						</tr>
+						<tr>
+							<th>s</th>
+							<td>s</td>
+						</tr>
+						<tr>
+							<th>s</th>
+							<td>s</td>
+						</tr>
+						<tr>
+							<th>s</th>
+							<td>s</td>
+						</tr>
+						<tr>
+							<th>s</th>
+							<td>s</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</section>
+	);
+};
