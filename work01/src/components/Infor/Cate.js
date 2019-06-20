756cad411658c648f072d4f20f70c19a97f31f29
謝政宏 <b3194359@gmail.com>
2019-06-20 22:21:07 +0800

    I'am so tired

--- work01/src/components/Infor/Cate.js
+++ work01/src/components/Infor/Cate.js
@@ -8,6 +8,23 @@ export const InforCats = ({ className = '', link = '', text = '' }) => (
 	</li>
 );
 
+export const InforLists = ({ className1 = '',className2 = '',link = '',imgSrc = '' ,date = '' ,text1 = '' ,text2 = '' }) => (
+  <article className={className1}>
+     <a className={className2} href={link}>
+      <figure className="thumb">
+        <img src={ imgSrc } width="90" height="90" alt="サムネイル">
+      </figure>
+      <div className="inner">
+        <div className="sub-info">
+           <p className="date">{ date }</p>
+           <p className="cat"><span>{ text1 }</span></p>
+        </div>
+        <h3 className="post-ttl">{ text2 }</h3>
+      </div>
+     </a>
+  </article>
+);
+
 export default () => {
 	return (
     <div className="cate">
@@ -21,6 +38,11 @@ export default () => {
           <ul>
             {[ ...Array(6).keys() ].map((arr) => <InforCats key={arr}></InforCats>)}
           </ul>
+        </div>
+      </div>
+      <div className="col-12">
+        <div className="information-list">
+            {[ ...Array(16).keys() ].map((arr) => <InforLists key={arr}></InforLists>)}
         </div>
       </div>
     </div>
